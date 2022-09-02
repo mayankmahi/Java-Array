e1ed053da99efb89e6225d27a23bb7a8e73c90a8
mayankmahi <mkmahi1198@gmail.com>
2022-09-02 12:03:24 +0530

    For-Eacg Java Array

---
+++ src/TestArray2.java
@@ -0,0 +1,10 @@
+// For-Each loop for array.
+public class TestArray2 {
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5};
+
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+}
