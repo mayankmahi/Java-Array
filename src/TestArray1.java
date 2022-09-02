69128df62085953bedc33df7c84ba63ae3cf6ae9
mayankmahi <mkmahi1198@gmail.com>
2022-09-02 11:42:45 +0530

    Java Array Declaration

---
+++ src/TestArray1.java
@@ -0,0 +1,11 @@
+//Declaration, Instantiation, and Initialization of Java Array
+// int a[] = {1,2,3,4,5};
+
+public class TestArray1 {
+    public static void main(String args[]){
+        int a[] = {1,2,3,4,5};
+        for(int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
